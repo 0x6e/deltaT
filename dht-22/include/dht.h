de0d51b9cfcc96a3c779cfdc9e509b7dfd5336da
0x6e <0x6e@users.noreply.github.com>
2015-06-11 21:18:47 +0100

    Changed to DHT source from https://github.com/mathew-hall/esp8266-dht/

---
+++ dht-22/include/dht.h
@@ -0,0 +1,20 @@
+// dht.h
+// From: https://github.com/mathew-hall/esp8266-dht/ - 0x6e 11/06/2015
+
+enum sensor_type{
+	SENSOR_DHT11,SENSOR_DHT22
+};
+
+struct sensor_reading{
+	float temperature;
+	float humidity;
+	const char* source;
+	uint8_t sensor_id[16];
+	BOOL success;
+};
+
+
+void ICACHE_FLASH_ATTR DHT(void);
+struct sensor_reading * ICACHE_FLASH_ATTR readDHT(int force);
+void DHTInit(enum sensor_type, uint32_t polltime);
+
