cb4f769f02601a2e85ee0b3c5f6ec55d14de1179
0x6e <0x6e@users.noreply.github.com>
2015-07-01 18:51:27 +0100

    ESP8266 gets connects and gets IP address

--- client/src/main.c
+++ client/src/main.c
@@ -3,6 +3,30 @@
 #include "user_interface.h"
 #include "wifi_settings.h"
 
+os_timer_t ip_check_timer;
+
+void check_ip() {
+  os_timer_disarm(&ip_check_timer);
+  os_printf("Checking ip...\r\n");
+
+  struct ip_info ipconfig;
+  wifi_get_ip_info(STATION_IF, &ipconfig);
+
+  if (wifi_station_get_connect_status() == STATION_GOT_IP)
+  {
+    os_printf("Got IP: ");
+    uint8 buf[20];
+    os_bzero(buf, sizeof(buf));
+    os_printf(IPSTR, IP2STR(&ipconfig.ip));
+  }
+  else
+  {
+    os_printf("Failed to get IP - retrying.");
+    os_timer_arm(&ip_check_timer, 10000, 0);
+  }
+}
+
+
 void user_init(void) {
   uart_init(BIT_RATE_115200, BIT_RATE_115200);
 
@@ -17,5 +41,11 @@ void user_init(void) {
   os_memcpy(&stationConfig.ssid, SSID, 32);
   os_memcpy(&stationConfig.password, PASSWORD, 64);
   wifi_station_set_config(&stationConfig);
+
+  // Start IP check timer
+  os_timer_disarm(&ip_check_timer);
+  os_timer_setfn(&ip_check_timer, (os_timer_func_t *)check_ip, NULL);
+  os_timer_arm(&ip_check_timer, 10000, 0);
 }
+
 
